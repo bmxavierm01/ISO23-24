58cb3bafba61e28660eed0105dc72fdd55800a93
bmxavierm01 <150673323+bmxavierm01@users.noreply.github.com>
2023-11-14 13:56:10 +0100

    Add files via upload

---
+++ Procesos_en_lotes/mkprofile.bat
@@ -0,0 +1,15 @@
+@echo off
+
+REM Script que genera un arrbol de directorios
+REM
+REM Autor: Bruno Moutinho
+REM Fecha: 13-11-2023
+
+mkdir %userprofile%\asir\docs %userprofile%\asir\pract %userprofile%\asir\exam
+mkdir %userprofile%\dam\docs  %userprofile%\dam\pract %userprofile%\dam\exam
+mkdir %userprofile%\smr\docs  %userprofile%\smr\pract %userprofile%\smr\exam
+mkdir %userprofile%\daw\docs %userprofile%\daw\pract %userprofile%\daw\exam
+mkdir %userprofile%\ceti\docs %userprofile%\ceti\pract %userprofile%\ceti\exam
+
+
+
